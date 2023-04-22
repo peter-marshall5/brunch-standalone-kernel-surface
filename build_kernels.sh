fa50b144ca3ffaf6e71e430694a316539034223a
Peter Marshall <petmshall@gmail.com>
2023-04-22 11:09:26 -0400

    Build the kernel with Clang LTO

--- build_kernels.sh
+++ build_kernels.sh
@@ -14,7 +14,7 @@ mkdir -p rootc/packages
 
 for kernel in $kernels; do
 	echo "Building kernel $kernel"
-	KCONFIG_NOTIMESTAMP=1 KBUILD_BUILD_TIMESTAMP='' KBUILD_BUILD_USER=chronos KBUILD_BUILD_HOST=localhost make -C "./kernels/$kernel" -j"$NTHREADS" O=out || { echo "Kernel build failed"; exit 1; }
+	KCONFIG_NOTIMESTAMP=1 KBUILD_BUILD_TIMESTAMP='' KBUILD_BUILD_USER=chronos KBUILD_BUILD_HOST=localhost make LLVM=1 -C "./kernels/$kernel" -j"$NTHREADS" O=out || { echo "Kernel build failed"; exit 1; }
 	rm -f "./kernels/$kernel/out/source"
 	#if [ -f /persist/keys/brunch.priv ] && [ -f /persist/keys/brunch.pem ]; then
 	#	echo "Signing kernel $kernel"
@@ -22,6 +22,6 @@ for kernel in $kernels; do
 	#	sbsign --key /persist/keys/brunch.priv --cert /persist/keys/brunch.pem "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" --output "./kernels/$kernel/out/arch/x86/boot/bzImage" || { echo "Kernel signing failed"; exit 1; }
 	#fi
 	cp "./kernels/$kernel/out/arch/x86/boot/bzImage" "./rootc/kernel-$kernel"
-	make -C "./kernels/$kernel" -j"$NTHREADS" O=out INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH="./kmods" modules_install
+	make LLVM=1 -C "./kernels/$kernel" -j"$NTHREADS" O=out INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH="./kmods" modules_install
 	tar zcf rootc/packages/kernel-"$kernel"-brunch-sebanc.tar.gz -C "./kernels/$kernel/out/kmods/" . --owner=0 --group=0
 done
